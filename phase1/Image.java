92cb039d6eece72499532f371d2bc0ad0c037c2a
Xingyu Wang <kara.wang@mail.utoronto.ca>
2017-11-12 16:26:45 -0500

    Image: Added JavaDocs for instance variables, added a fullname iv, implements serializable

--- phase1/Image.java
+++ phase1/Image.java
@@ -1,17 +1,43 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Observable;
 import java.util.Observer;
 
-public class Image extends Observable {
+public class Image extends Observable implements Serializable {
 
+  /**
+   * the name part of file's name.
+   */
   private String name;
+
+  /**
+   * The full name of file.
+   */
+  private String fullName;
+
+  /**
+   * this image file.
+   */
   private File file;
+
+  /**
+   * An ArrayList of all the tags of this image.
+   */
   private ArrayList<String> currentTags;
+
+
+  /**
+   * The observers.
+   */
   private ArrayList<Observer> observers = new ArrayList<>();
 
   public Image(File file) {
-    this.name = file.getName();
     this.file = file;
     currentTags = new ArrayList<>();
   }
@@ -27,7 +53,6 @@ public class Image extends Observable {
     File newFile = new File(path + File.separator + name);
     if (file.renameTo(newFile)) {
       System.out.println("File rename success");
-      ;
     } else {
       System.out.println("File rename failed");
     }
