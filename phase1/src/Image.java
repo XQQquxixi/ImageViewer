6e24920550a304d5821c2bd89deda9812c14f57f
Xingyu Wang <kara.wang@mail.utoronto.ca>
2017-11-14 11:00:30 -0500

    Image constructor to handle special occasions, writeToFile and readFromFile in Image, and move method in Image.

--- phase1/src/Image.java
+++ phase1/src/Image.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Observable;
@@ -27,11 +30,48 @@ public class Image extends Observable implements Serializable {
    */
   private ArrayList<Observer> observers = new ArrayList<>();
 
-  public Image(File file) {
-    this.name = file.getName();
+  public Image(File file) throws IOException, ClassNotFoundException {
     this.file = file;
     currentTags = new ArrayList<>();
+    this.name = file.getName();
+
+    if (file.getName().contains("@")) {
+      // Modified before
+      String path = file.getAbsolutePath();
+      File ser =new File(
+          path.substring(0, path.lastIndexOf(File.separator) + 1)+this.name+".ser");
+      if (ser.exists() && ser.isFile()) {
+        // There is a .ser file
+        Image reload = readFromFile(ser);
+        this.name = reload.name;
+        this.currentTags = reload.currentTags;
+      } else {
+        // There isn't one but the file name retains info of tags
+        ArrayList<String> tags = new ArrayList<>(Arrays.asList(file.getName().split("@")));
+        this.name = tags.get(0).replaceAll("\\s", "");
+        currentTags.addAll(tags.subList(1, tags.size()));
+        writeToFile();
+      }
+    }
   }
+
+  private void writeToFile() {
+    try {
+      ObjectOutputStream serOut =
+          new ObjectOutputStream(new FileOutputStream("name" + ".ser"));
+      serOut.writeObject(this);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private Image readFromFile(File ser) throws IOException, ClassNotFoundException {
+    ObjectInputStream serIn = new ObjectInputStream(new FileInputStream(ser));
+    Image result = (Image) serIn.readObject();
+    serIn.close();
+    return result;
+  }
+
 
   public String getName() {
     return name;
@@ -73,29 +113,40 @@ public class Image extends Observable implements Serializable {
     return name;
   }
 
-   public void addTag(String tag) throws IOException {
-   if (currentTags.contains(tag)){
-   System.out.println("This tag is already in here!");
-   } else {
-   currentTags.add(tag);
-   this.setName(this.name + " @" + tag);
-   if (!TagManager.tagList.contains(tag)){
-   TagManager.addTag(tag);
-   }
-   }
-   }
+  public void addTag(String tag) throws IOException {
+    if (currentTags.contains(tag)) {
+      System.out.println("This tag is already in here!");
+    } else {
+      currentTags.add(tag);
+      this.setName(this.name + " @" + tag);
+      if (!TagManager.tagList.contains(tag)) {
+        TagManager.addTag(tag);
+      }
+    }
+  }
 
-   public void deleteTag(String tag){
-   if (currentTags.contains(tag)){
-   currentTags.remove(tag);
-   int index = name.lastIndexOf(" @"+tag);
-   String newName = name.substring(0, index) +
-   name.substring(index + (" @" + tag).length()+1, name.length());
-   this.setName(newName);
-   } else {
-   System.out.println("No such tag in this photo!");
-   }
-   }
+  public void deleteTag(String tag) {
+    if (currentTags.contains(tag)) {
+      currentTags.remove(tag);
+      int index = name.lastIndexOf(" @" + tag);
+      String newName = name.substring(0, index) +
+          name.substring(index + (" @" + tag).length() + 1, name.length());
+      this.setName(newName);
+    } else {
+      System.out.println("No such tag in this photo!");
+    }
+  }
+
+  public void move(String newPath) throws IOException {
+    Path oldPath = file.toPath();
+    Files.move(oldPath, Paths.get(newPath));
+    file = new File(newPath);
+    String serPath =
+        oldPath.toString().substring(0, oldPath.toString().lastIndexOf(File.separator)+1)
+            + name + ".ser";
+    Files.delete(Paths.get(serPath));
+    notifyObservers(name);
+  }
 
 
 }
--- phase1/src/LogObserver.java
+++ phase1/src/LogObserver.java
@@ -3,91 +3,94 @@ import java.util.*;
 
 public class LogObserver implements Observer {
 
-    private Image image;
-    private StringBuilder logs = new StringBuilder();
-    private File file;
+  private Image image;
+  private StringBuilder logs = new StringBuilder();
+  private File file;
 
 
-    public LogObserver(Image image) throws IOException, ClassNotFoundException {
-        this.image = image;
-        this.image.addObserver(this);
+  public LogObserver(Image image) throws IOException, ClassNotFoundException {
+    this.image = image;
+    this.image.addObserver(this);
 
-        String absolutePath = image.getFile().getAbsolutePath();
-        String filePath = absolutePath.substring(0, absolutePath.lastIndexOf(File.separator)) + File.separator + "log_" + image.getName() + ".ser";
-        this.file = new File(filePath);
+    String absolutePath = image.getFile().getAbsolutePath();
+    String filePath =
+        absolutePath.substring(0, absolutePath.lastIndexOf(File.separator)) + File.separator
+            + "log_" + image.getName() + ".ser";
+    this.file = new File(filePath);
 
-        if (file.exists()) {
-            readFromFile(filePath);
-        } else {
-            file.createNewFile();
-        }
+    if (file.exists()) {
+      readFromFile(filePath);
+    } else {
+      file.createNewFile();
     }
+  }
 
-    public void readFromFile(String filePath) throws ClassNotFoundException {
+  public void readFromFile(String filePath) throws ClassNotFoundException {
 
-        try {
-            InputStream file = new FileInputStream(filePath);
-            InputStream buffer = new BufferedInputStream(file);
-            ObjectInput input = new ObjectInputStream(buffer);
+    try {
+      InputStream file = new FileInputStream(filePath);
+      InputStream buffer = new BufferedInputStream(file);
+      ObjectInput input = new ObjectInputStream(buffer);
 
-
-            logs = (StringBuilder) input.readObject();
-            input.close();
-        } catch (IOException ex) {
-            System.out.println("Cannot read from input.");
-        }
+      logs = (StringBuilder) input.readObject();
+      input.close();
+    } catch (IOException ex) {
+      System.out.println("Cannot read from input.");
     }
+  }
 
-    public void add(Image image, String nameOld, long time) {
-        String newLog = nameOld + " " + image.getName() + " " + Long.toString(time);
-        logs.append(newLog);
-        logs.append(System.lineSeparator());
+  public void add(Image image, String nameOld, long time) {
+    String newLog = nameOld + " " + image.getName() + " " + Long.toString(time);
+    logs.append(newLog);
+    logs.append(System.lineSeparator());
 
-        // LogObserver the addition of a student.
-        System.out.println("Added a new renaming " + image.toString());
+    // LogObserver the addition of a student.
+    System.out.println("Added a new renaming " + image.toString());
+  }
+
+  /**
+   * Writes the students to file at filePath.
+   *
+   * @param filePath the file to write the records to
+   */
+  public void saveToFile(String filePath) throws IOException {
+
+    OutputStream file = new FileOutputStream(filePath);
+    OutputStream buffer = new BufferedOutputStream(file);
+    ObjectOutput output = new ObjectOutputStream(buffer);
+
+    // serialize the Map
+    output.writeObject(logs);
+    output.close();
+  }
+
+
+  public void update(Observable o, Object oldName) {
+    Date date = new Date();
+    long time = date.getTime();
+
+    // add new log entry
+    if (!((Image) o).getName().equals(oldName)) {
+      add((Image) o, (String) oldName, time);
+      try {
+        saveToFile(file.getAbsolutePath());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+    // update log name
+    String absolutePath = ((Image) o).getFile().getAbsolutePath();
+    String fileName = ((Image) o).getName();
+    File newFile = new File(
+        absolutePath.substring(0, absolutePath.lastIndexOf(File.separator)) + File.separator
+            + "log_" + fileName + ".ser");
+    file.renameTo(newFile);
+    file = newFile;
 
-    /**
-     * Writes the students to file at filePath.
-     * @param filePath the file to write the records to
-     * @throws IOException
-     */
-    public void saveToFile(String filePath) throws IOException {
-
-        OutputStream file = new FileOutputStream(filePath);
-        OutputStream buffer = new BufferedOutputStream(file);
-        ObjectOutput output = new ObjectOutputStream(buffer);
-
-        // serialize the Map
-        output.writeObject(logs);
-        output.close();
-    }
+  }
 
 
-    public void update(Observable o, Object oldName) {
-        Date date = new Date();
-        long time = date.getTime();
-
-        // add new log entry
-        if (!((Image) o).getName().equals(oldName)) {
-            add((Image) o, (String) oldName, time);
-            try {
-                saveToFile(file.getAbsolutePath());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        // update log name
-        String absolutePath = ((Image) o).getFile().getAbsolutePath();
-        String fileName = ((Image) o).getName();
-        File newFile = new File(absolutePath.substring(0, absolutePath.lastIndexOf(File.separator)) + File.separator + "log_" + fileName + ".ser");
-        file.renameTo(newFile);
-        file = newFile;
-
-    }
-
-
-    public StringBuilder getLogs() {
-        return logs;
-    }
+  public StringBuilder getLogs() {
+    return logs;
+  }
 }
