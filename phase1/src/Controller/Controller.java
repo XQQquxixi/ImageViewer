df34eb3b5a837630e9ff67c92f2cf8cf2145a6a4
Xueqing Yang <xueqing.yang@mail.utoronto.ca>
2017-11-17 12:38:40 -0500

    - added an Alert if the directory is not valid. - Store all selected files's name and File in a map for later look up.

--- phase1/src/Controller/Controller.java
+++ phase1/src/Controller/Controller.java
@@ -2,13 +2,17 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Controller {
@@ -19,20 +23,32 @@ public class Controller {
     private ImageView iv1;
 
     @FXML
-    private ListView listView;
+    private ListView<String> listView;
 
-    public void Button1Action(ActionEvent event) {
-        FileChooser fc = new FileChooser();
-        fc.setInitialDirectory(new File("//Users//yangxueqing//Desktop//ProImageTry"));
-        fc.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
-        File selectedFile = fc.showOpenDialog(null);
-        if (selectedFile != null) {
-            Image image = new Image(selectedFile.toURI().toString());
-            iv1.setImage(image);
-            listView.getItems().add(selectedFile.getName());
+    @FXML
+    private TextField initDirectory;
+
+    private Map<String, File> nameToPath = new HashMap<>();
+
+    public void Button1Action(ActionEvent event) throws Exception{
+        File directory = new File(initDirectory.getText());
+        if (directory.exists() && directory.isDirectory()) {
+            FileChooser fc = new FileChooser();
+            fc.setInitialDirectory(directory);
+            fc.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
+            File selectedFile = fc.showOpenDialog(null);
+            if (selectedFile != null) {
+                nameToPath.put(selectedFile.getName(), selectedFile);
+                Image image = new Image(selectedFile.toURI().toString());
+                iv1.setImage(image);
+                listView.getItems().add(selectedFile.getName());
+            }
         } else {
-            System.out.println("not valid");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Uh-oh");
+            alert.setContentText("Wrong Directory!!! Please check and enter again.");
+            alert.showAndWait();
         }
     }
 
