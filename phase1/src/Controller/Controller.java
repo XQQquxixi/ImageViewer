1813b6b6d4c881e455e2c877614e6df1abf9d4bf
Xueqing Yang <xueqing.yang@mail.utoronto.ca>
2017-11-17 12:40:56 -0500

    added ButtonOkAction: when clicking ok, it will open a new window with operation on the current selected image.

--- phase1/src/Controller/Controller.java
+++ phase1/src/Controller/Controller.java
@@ -2,6 +2,8 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -9,14 +11,21 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class Controller {
+
+    private static final double DISTANCE = 50.0;
+
     @FXML
     private Button btn1;
 
@@ -28,6 +37,9 @@ public class Controller {
 
     @FXML
     private TextField initDirectory;
+
+    @FXML
+    private Button ok;
 
     private Map<String, File> nameToPath = new HashMap<>();
 
@@ -56,6 +68,26 @@ public class Controller {
     public void MouseClickList(MouseEvent event) {
         Image image = new Image(nameToPath.get(listView.getSelectionModel().getSelectedItem()).toURI().toString());
         iv1.setImage(image);
+    }
+
+    public void ButtonOkAction(ActionEvent event) throws IOException {
+//        ObservableList<String> pictures;
+//        pictures = listView.getSelectionModel().getSelectedItems();
+//        for (String pic : pictures) {
+        Window window = ok.getScene().getWindow();
+        Stage primaryStage = new Stage();
+        primaryStage.initOwner(window);
+        FXMLLoader loader = new FXMLLoader();
+        Pane root = loader.load(getClass().getResource("ImageViewer.fxml").openStream());
+//        ImageViewer imageView = loader.getController();
+//        imageView.GetImage(nameToFile.get(listView.getSelectionModel().getSelectedItem()));
+        primaryStage.setTitle("Image Viewer");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setX(window.getX() + DISTANCE);
+        primaryStage.setY(window.getY() + DISTANCE);
+        primaryStage.show();
+//        }
+
     }
 
 }
