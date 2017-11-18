5ff0178d987586cc8c3ed1ce420723a124c193b4
xuqiqi <frances.qiqi.xu@gmail.com>
2017-11-18 18:50:50 -0500

    Removed redundant variables

--- phase1/src/Utility/FileManager.java
+++ phase1/src/Utility/FileManager.java
@@ -6,16 +6,14 @@ public class FileManager {
     public ObjectOutput saveToFile(String filePath) throws IOException {
         OutputStream file = new FileOutputStream(filePath);
         OutputStream buffer = new BufferedOutputStream(file);
-        ObjectOutput output = new ObjectOutputStream(buffer);
-        return output;
+        return  new ObjectOutputStream(buffer);
     }
 
 
     public ObjectInput readFromFile(String path) throws ClassNotFoundException, IOException {
         InputStream file = new FileInputStream(path);
         InputStream buffer = new BufferedInputStream(file);
-        ObjectInput input = new ObjectInputStream(buffer);
-        return input;
+        return new ObjectInputStream(buffer);
     }
 
 }
