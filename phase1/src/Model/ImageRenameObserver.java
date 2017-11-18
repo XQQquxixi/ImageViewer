2d21918c9527a0c2f8ea6b432d09733e8dcc2fed
Xingyu Wang <kara.wang@mail.utoronto.ca>
2017-11-17 22:42:56 -0500

    Added JavaDoc and reformatted code.

--- phase1/src/Model/ImageRenameObserver.java
+++ phase1/src/Model/ImageRenameObserver.java
@@ -8,16 +8,37 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * An Observer for renaming Image.
+ */
 public class ImageRenameObserver implements Observer {
 
+  /**
+   * The Image this observer is recording renaming logs of.
+   */
   private Image image;
+  /**
+   * The log of re-namings done. Each line composes of the old name, the new name, and the
+   * timestamp, all separated by comma. Each entry separated by a linebreak.
+   */
   private StringBuilder contents = new StringBuilder();
+  /**
+   * A Logger.
+   */
   private static final Logger logger =
       Logger.getLogger(ImageRenameObserver.class.getName());
+  /**
+   * A Console Handler.
+   */
   private static final Handler consoleHandler = new ConsoleHandler();
 
 
-  public ImageRenameObserver(Image image) throws IOException, ClassNotFoundException {
+  /**
+   * Initialize an ImageRenameObserver with an association with an Image object.
+   *
+   * @param image the Image associated.
+   */
+  public ImageRenameObserver(Image image) {
     this.image = image;
     this.image.addObserver(this);
     logger.setLevel(Level.ALL);
@@ -25,6 +46,14 @@ public class ImageRenameObserver implements Observer {
     logger.addHandler(consoleHandler);
   }
 
+  /**
+   * Add an entry of image's renaming in the contents log with nameOld as the old name and time as
+   * the time modified.
+   *
+   * @param image this image that was renamed.
+   * @param nameOld the previous name before renaming.
+   * @param time the time when the renaming occurred.
+   */
   public void add(Image image, String nameOld, String time) {
     String newLog = nameOld + "," + image.getName() + "," + time;
     contents.append(newLog);
@@ -33,23 +62,45 @@ public class ImageRenameObserver implements Observer {
     logger.log(Level.FINE, "Added a new renaming " + image.toString());
   }
 
-  public void update(Observable o, Object oldName) {
+  /**
+   * This method is called whenever the observed object is changed.
+   *
+   * @param image the observable image that is changed
+   * @param oldName the old name before renaming.
+   */
+  @Override
+  public void update(Observable image, Object oldName) {
     Date now = new Date();
     SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
     String date = dt.format(now);
-    add((Image) o, (String) oldName, date);
+    add((Image) image, (String) oldName, date);
   }
 
 
+  /**
+   * Return the String version of the log stored in contents.
+   *
+   * @return the log recorded so far in String.
+   */
   public String getLogs() {
     return contents.toString();
   }
 
+  /**
+   * Return an ArrayList of the log where each item is an entry in the log.
+   *
+   * @return An ArrayList of entries in log.
+   */
   private ArrayList<String> getListOfLog() {
     String logs = this.getLogs();
     return new ArrayList<>(Arrays.asList(logs.split(System.lineSeparator())));
   }
 
+  /**
+   * Return a list of past name the associated Image image had.
+   *
+   * @return An ArrayList of past names.
+   */
   public ArrayList<String> getPastNames() {
     ArrayList<String> listOfLog = getListOfLog();
     listOfLog.remove(0);
--- phase1/src/Model/TagManager.java
+++ phase1/src/Model/TagManager.java
@@ -7,16 +7,33 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * A Tag Manager.
+ */
 public class TagManager {
 
-  public static ArrayList<String> tagList;
+  /**
+   * The list of all existing tags.
+   */
+  public static ArrayList<String> tagList = new ArrayList<>();
+  /**
+   * The string path of the serialized file of this TagManager.
+   */
   private static String filePath = "./tm.ser";
+  /**
+   * A Logger.
+   */
   private static final Logger logger =
-          Logger.getLogger(TagManager.class.getName());
+      Logger.getLogger(TagManager.class.getName());
+  /**
+   * A Console Handler.
+   */
   private static final Handler consoleHandler = new ConsoleHandler();
 
+  /**
+   * Initialize a TagManager and creates a serialized file.
+   */
   public TagManager() throws ClassNotFoundException, IOException {
-    tagList = new ArrayList<>();
     logger.setLevel(Level.ALL);
     consoleHandler.setLevel(Level.ALL);
     logger.addHandler(consoleHandler);
@@ -25,16 +42,27 @@ public class TagManager {
     if (file.exists()) {
       readFromFile(filePath);
     } else {
-      if(file.createNewFile()) {
+      if (file.createNewFile()) {
         logger.log(Level.FINE, "created tag.ser");
       }
     }
   }
 
+  /**
+   * Return a copy of tagList.
+   *
+   * @return a copy of tagList.
+   */
   public ArrayList<String> getTagList() {
     return new ArrayList<>(tagList);
   }
 
+  /**
+   * Add a tag into tagList then update the serialized file if it does not exist in it. Otherwise
+   * add a WARNING log.
+   *
+   * @param tag the added tag
+   */
   public static void addTag(String tag) throws IOException {
     if (!tagList.contains(tag)) {
       tagList.add(tag);
@@ -45,27 +73,43 @@ public class TagManager {
 
   }
 
+  /**
+   * Remove a tag from tagList then update the serialized file if it exists in it. Otherwise add a
+   * WARNING log.
+   *
+   * @param tag the tag to delete
+   */
   public static void removeTag(String tag) throws IOException {
     if (tagList.contains(tag)) {
       tagList.remove(tag);
       saveToFile(filePath);
     } else {
-      logger.log(Level.WARNING,"No such tag.");
+      logger.log(Level.WARNING, "No such tag.");
     }
 
   }
 
+  /**
+   * Save this TagManager to a serialized file to filePath.
+   *
+   * @param filePath the path to save the file.
+   */
   public static void saveToFile(String filePath) throws IOException {
 
     OutputStream file = new FileOutputStream(filePath);
     OutputStream buffer = new BufferedOutputStream(file);
     ObjectOutput output = new ObjectOutputStream(buffer);
 
-    // serialize the Map
     output.writeObject(tagList);
     output.close();
   }
 
+  /**
+   * Read a TagManager from the serialized file with path and update this TagManger's tagList with
+   * the read TagManager's.
+   *
+   * @param path the path of this file to read.
+   */
   public static void readFromFile(String path) throws ClassNotFoundException {
 
     try {
@@ -73,10 +117,10 @@ public class TagManager {
       InputStream buffer = new BufferedInputStream(file);
       ObjectInput input = new ObjectInputStream(buffer);
 
-      tagList = (ArrayList<String>) input.readObject();
+      tagList = ((TagManager) (input.readObject())).getTagList();
       input.close();
     } catch (IOException ex) {
-      logger.log(Level.WARNING,"Cannot find the file.");
+      logger.log(Level.WARNING, "Cannot find the file.");
     }
   }
 }
