f83fc19a8e1c14b6b4f1cbd3c890b96531a7f2d5
Xueqing Yang <xueqing.yang@mail.utoronto.ca>
2017-11-19 02:45:11 -0500

    fixed some bugs in RenameController

--- phase1/src/Controller/RenameController.java
+++ phase1/src/Controller/RenameController.java
@@ -1,14 +1,17 @@
 package Controller;
 
 import Model.Image;
+import Model.ImageManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.File;
 import java.io.IOException;
@@ -27,6 +30,9 @@ public class RenameController implements Initializable{
     private ChoiceBox<String> prevNames;
 
     @FXML
+    private Button ok;
+
+    @FXML
     private Button cancel;
 
 //    private ArrayList<String> listOfPrevNames = new ArrayList<>();
@@ -36,6 +42,9 @@ public class RenameController implements Initializable{
     void getImage(Image image) {
         this.image = image;
         System.out.println(image.toString());
+        ArrayList<String> listOfPrevNames = ImageManager.getPastName(image.getFile().getAbsolutePath());
+        prevNames.getItems().addAll(listOfPrevNames);
+        curName.setText(image.getName());
     }
 
     public void TypeName(ActionEvent event) {
@@ -46,8 +55,20 @@ public class RenameController implements Initializable{
         curName.setText(prevNames.getValue());
     }
 
-    public void ButtonOkAction(ActionEvent event) {
-        image.setName(curName.getText());
+    public void ButtonOkAction(ActionEvent event) throws IOException {
+        //image.setName(curName.getText());
+        try {
+            image = ImageManager.renameImage(image.getFile().getAbsolutePath(), curName.getText());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        FXMLLoader loader = new FXMLLoader();
+        loader.load(getClass().getResource("ImageView.fxml").openStream());
+        ImageViewController controller = loader.getController();
+        controller.initData(image);
+        Window window = ok.getScene().getWindow();
+        Stage stage = (Stage) window;
+        stage.close();
     }
 
     public void ButtonCancelAction(ActionEvent event) {
@@ -57,7 +78,7 @@ public class RenameController implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO: should get the list of previous names from model
-        Image listOfPrevNames = image;
+
         //        ArrayList<String> listOfPrevNames = image.getImageRenameObserver().getPastNames();
 
 //        logs.remove(0);
@@ -67,6 +88,6 @@ public class RenameController implements Initializable{
 //        listOfPrevNames.add("haha");
 //        listOfPrevNames.add("hehe");
 //        listOfPrevNames.add("heihei");
-//        prevNames.getItems().addAll(listOfPrevNames);
+
     }
 }
